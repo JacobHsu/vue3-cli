28d7696f80a6e5e5159306d9a60649fc23371f9c
jacobhsu <jacob.hsu.tw@gmail.com>
2020-12-26 14:53:57 +0800

    web-forms 43

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <SignupForm />
   <ReactionTimer/>
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
@@ -7,12 +8,14 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import ReactionTimer from './components/ReactionTimer.vue'
+import SignupForm from './components/SignupForm'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    ReactionTimer
+    ReactionTimer,
+    SignupForm 
   }
 }
 </script>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,111 @@
+<template>
+  <form>
+    <label>Email:</label>
+    <input type="email" v-model="email" required>
+
+    <label>Password:</label>
+    <input type="password" v-model="password" required>
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+
+    <label>Skills (press alt + comma to add):</label>
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+    <div v-for="skill in skills" :key="skill" class="pill">
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
+
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required>
+      <label>Accept terms and conditions</label>
+    </div>
+  </form>
+
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Your role: {{ role }}</p>
+  <p>Terms accepted: {{ terms }}</p>
+</template>
+
+<script>
+// challenge
+//   - when a user clicks on a skill, delete that skill
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      role: 'developer',
+      terms: false,
+      skills: [],
+      tempSkill: '',
+    }
+  },
+  methods: {
+    addSkill($event) {
+      if($event.key === ',' && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill)
+        }
+        this.tempSkill = ''
+      }
+    },
+    deleteSkill(skill) {
+      this.skills = this.skills.filter(item => {
+        return skill !== item
+      })
+    }
+  }
+}
+</script>
+
+<style>
+  form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+  }
+  label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+  input, select {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+  input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+  }
+  .pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+  }
+</style>
