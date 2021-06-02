4ac3871bc4542a1de71b86acb1b7a9d4f1b58de1
jacobhsu <jacob.hsu.tw@gmail.com>
2021-06-02 09:58:18 +0800

    live-chat firebase config

---
+++ live-chat/src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+// https://console.firebase.google.com/u/0/project/vue3-cli setting live-chat
+const firebaseConfig = {
+    apiKey: "AIzaSyARZwCY5P_Z0WaQMEsSbzvgeDVhMmRbtBU",
+    authDomain: "vue3-cli.firebaseapp.com",
+    projectId: "vue3-cli",
+    storageBucket: "vue3-cli.appspot.com",
+    messagingSenderId: "838901975015",
+    appId: "1:838901975015:web:950dbc4454078aeed29f42"
+  };
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init firestore service
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, timestamp }
