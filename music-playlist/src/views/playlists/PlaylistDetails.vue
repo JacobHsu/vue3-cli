174532b4f9a8241e26583fad42cec17b97bf0ef3
jacobhsu <jacob.hsu.tw@gmail.com>
2021-07-30 09:36:50 +0800

    music: deleting images from storage

--- music-playlist/src/views/playlists/PlaylistDetails.vue
+++ music-playlist/src/views/playlists/PlaylistDetails.vue
@@ -9,7 +9,7 @@
       <h2>{{ playlist.title }}</h2>
       <p class="username">Created by {{ playlist.userName }}</p>
       <p class="description">{{ playlist.description }}</p>
-      <button v-if="ownership"  @click="handleDelete">Delete Playlist</button>
+      <button v-if="ownership" @click="handleDelete">Delete Playlist</button>
     </div>
 
     <!-- song list -->
@@ -20,10 +20,12 @@
 </template>
 
 <script>
+import useStorage from '@/composables/useStorage';
 import useDocument from '@/composables/useDocument';
 import getDocument from '@/composables/getDocument';
 import getUser from '@/composables/getUser';
 import { computed } from 'vue';
+import { useRouter } from 'vue-router'
 
 export default {
   props: ['id'],
@@ -31,6 +33,8 @@ export default {
     const { error, document: playlist } = getDocument('playlists', props.id);
     const { user } = getUser();
     const { deleteDoc } = useDocument('playlists', props.id);
+    const { deleteImage } = useStorage();
+    const router = useRouter();
 
     const ownership = computed(() => {
       return (
@@ -40,6 +44,8 @@ export default {
 
     const handleDelete = async () => {
       await deleteDoc();
+      await deleteImage(playlist.value.filePath);
+      router.push({ name: 'Home' });
     };
 
     return { error, playlist, ownership, handleDelete };
